d42f31ad8367e1cc1ae6940e83c4bcae9d566fa6
ZarasTM <p.zaras@wp.pl>
2019-05-20 16:26:30 +0200

    Interrupt added
    Added interruption handling

--- main/main.ino
+++ main/main.ino
@@ -1,11 +1,16 @@
 #include <Servo.h>
 #include "Wheels.h"
+#include "PinChangeInterrupt.h"
 
-// pins for sonar (HC-SR04)
+// Pins for rotations
+#define ROT_PIN_LEFT A0 
+#define ROT_PIN_RIGHT A1
+
+// Pins for sonar (HC-SR04)
 #define TRIG A4
 #define ECHO A5
 
-// servo pin (has to be PWM)
+// Servo pin (has to be PWM)
 #define SERVO 3
 
 // Max and min values for event horizon
@@ -13,23 +18,45 @@
 #define MIN_DIST 50
 #define MAX_DIST 100
 
+// Circut of weheels in milimeters
+#define CIRC 2125
+
 Servo serwo;
 
 Wheels w;
 
+// Speed manipulation variables
 int speedL, speedR, speedAll;
+
+// Current distance returned by sonar
 int dist;
+
+// Variables for stailizing ride
+volatile int lGap, rGap;
+
+// Variables for measuring distance
+int lRot, rRot; // Rotations per second
 
 void setup() {
   w.attach(9,8,10,6,7,5);
-  
+
+  // Set speed
   speedAll = 255;
   speedL = speedAll;
   speedR = speedAll;
 
+  // Set gaps
+  restartGaps();
+  restartRot();
+  
   w.setSpeed(speedAll);
   w.stop();
+  
+  // Interrupt pins
+  pinMode(ROT_PIN_LEFT, INPUT); 
+  pinMode(ROT_PIN_RIGHT, INPUT); 
 
+  // Sonar pins
   pinMode(TRIG, OUTPUT);    // TRIG starts sonar
   pinMode(ECHO, INPUT);     // ECHO gets returning signal
 
@@ -38,11 +65,22 @@ void setup() {
   serwo.write(90);
 
   Serial.begin(9600);
+
+  attachPCINT(digitalPinToPCINT(ROT_PIN_LEFT), increment, CHANGE);
+  attachPCINT(digitalPinToPCINT(ROT_PIN_RIGHT), increment, CHANGE);
 }
 
 void loop() {
   checkPath();
+  Serial.print(rGap);
+  Serial.print(" ");  
+  Serial.println(lGap);
   delay(200);
+}
+
+void increment(){
+ if( (PINC & (1 << PC0)) ) lGap++;
+ if( (PINC & (1 << PC1)) ) rGap++;
 }
 
 void checkPath(){
@@ -78,4 +116,14 @@ int lookAndTellDistance(byte angle) {
   distance = tot/58;
   
   return distance;
+}
+
+void restartGaps(){
+  rGap = 0;
+  lGap = 0;
+}
+
+void restartRot(){
+  rRot = 0;
+  lRot = 0;
 }
